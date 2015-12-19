2245335e1dea12ab1e066d26e69653c021669a71
Philip Ford <pford68@earthlink.net>
2015-12-18 21:13:24 -0500

    Renamed dist.js to deploy.js and changed zip to create a deployable node app

--- resources/tasks/deploy.js
+++ resources/tasks/deploy.js
@@ -7,9 +7,15 @@ var zip = require('gulp-zip');
 var rev = require('gulp-rev');  // For appending timestamps to filenames
 var project = require('../project.json');
 
+var dist = [
+    'build/*',
+    '*.json',
+    'config/*',
+    'server.js'
+];
 
 gulp.task('zip', function () {
-    return gulp.src('build/*')
+    return gulp.src(dist, { base: "." })
         .pipe(zip(project.name + ".zip"))
         .pipe(rev())
         .pipe(gulp.dest('dist'));
